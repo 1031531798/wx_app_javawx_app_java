d43e84c61f919b9f085fb2656249980655d43418
1031531798 <1031531798@qq.com>
2021-01-29 08:45:38 +0800

    后台文件

---
+++ src/test/java/com/wx_app/wx_app/WxAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx_app.wx_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
